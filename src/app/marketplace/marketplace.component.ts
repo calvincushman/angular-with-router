29a558135d2a26be451cccc83c3bd59f28bfc428
Calvin  Cushman and Daniela Trulls <daniela@>
2017-07-10 16:36:04 -0700

    add dynamic routing to album details

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
   styleUrls: ['./marketplace.component.css']
 })
-export class MarketplaceComponent implements OnInit {
+export class MarketplaceComponent{
+constructor(private router: Router){}
 
-  constructor() { }
+  albums: Album[] = [
+    new Album("Ylistis", "Cosmic Church", "2012 album by Finnish black metal band", 1),
+    new Album("Balance", "Swollen Members", "1998 hip hop album by Vancouver BC duo", 2),
+    new Album("Surfer Rosa", "The Pixies", "1992 record by american band", 3)
+  ];
 
-  ngOnInit() {
-  }
-
+  goToDetailPage(clickedAlbum: Album) {
+    this.router.navigate(['albums', clickedAlbum.id]);
+  };
 }
